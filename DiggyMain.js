ca21eb21d823ec27a69ef4aae0f494309819a516
Z-Cartaa <zbrother@protonmail.com>
2024-10-18 08:35:18 +1100

    Update DiggyMain.js

--- DiggyMain.js
+++ DiggyMain.js
@@ -1,8 +1,14 @@
 // External Java Scripts for Diggy's Indie Dome - keep minmimal for load time please.
 
 // Start of Invite button interaction
-document.getElementById("inviteButton").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default link action
-    this.textContent = "TBC"; // Change the button text to "TBC"
+document.addEventListener("DOMContentLoaded", function() {
+    const inviteButton = document.getElementById("inviteButton");
+
+    if (inviteButton) {
+        inviteButton.addEventListener("click", function(event) {
+            event.preventDefault(); // Prevent the default link action
+            this.textContent = "TBC"; // Change the button text to "TBC"
+        });
+    }
 });
 // End of Invite button interaction
